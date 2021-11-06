ff28bf47244cbb0c2e0bc7ed9860f8e983c70a5d
Litsyshyn <71875485+Litsyshyn@users.noreply.github.com>
2021-11-06 20:23:36 +0200

    Build

---
+++ build/precache-manifest.22af0411e1b87d1f96594d044ee00a0d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accde38186ddcfcf1e328d2029865e51",
+    "url": "/psycounseling-front/index.html"
+  },
+  {
+    "revision": "684b7302873e48ef1301",
+    "url": "/psycounseling-front/static/css/main.b5313184.chunk.css"
+  },
+  {
+    "revision": "469b4331b7d758f34047",
+    "url": "/psycounseling-front/static/js/2.82ed724a.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/psycounseling-front/static/js/2.82ed724a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684b7302873e48ef1301",
+    "url": "/psycounseling-front/static/js/main.3230e149.chunk.js"
+  },
+  {
+    "revision": "c2fee972e8053ac6f3ac",
+    "url": "/psycounseling-front/static/js/runtime-main.0b912c60.js"
+  },
+  {
+    "revision": "172ae0f2ea438125a56678da29b5939d",
+    "url": "/psycounseling-front/static/media/Background_directions.172ae0f2.png"
+  },
+  {
+    "revision": "c375dfe63423637acd72a0462f0acab7",
+    "url": "/psycounseling-front/static/media/bg1.c375dfe6.png"
+  },
+  {
+    "revision": "a2d606c6936ace373db3572696c16986",
+    "url": "/psycounseling-front/static/media/child.a2d606c6.png"
+  },
+  {
+    "revision": "29329af854a47f7be616d7b347d116f0",
+    "url": "/psycounseling-front/static/media/family.29329af8.png"
+  },
+  {
+    "revision": "ee424b25b25de5106bb50a8e5405c246",
+    "url": "/psycounseling-front/static/media/logo.ee424b25.png"
+  },
+  {
+    "revision": "a9efbf1e47783824db346aa7fccdab36",
+    "url": "/psycounseling-front/static/media/trauma.a9efbf1e.png"
+  }
+]);
